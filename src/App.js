ced60db7cfb5b6734d02e619b12c0bbb7e740fa9
rjmcbain <rmcbain01@gmail.com>
2019-02-19 18:26:22 -0700

    creating store and actions

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Redirect } from "react-router-dom";
+import { Provider } from "react-redux";
 import "./App.css";
 
 import Navbar from "./components/Navbar";
 import RentalList from "./components/rental/RentalList";
 import RentalDetail from "./components/rental/RentalDetail";
 
+const store = require("./reducers/store").init();
+
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div>
-          <Navbar />
-          <div className="container">
-            <Route
-              exact
-              path="/"
-              render={() => {
-                return <Redirect to="rentals" />;
-              }}
-            />
-            <Route exact path="/rentals" component={RentalList} />
-            <Route exact path="/rentals/:id" component={RentalDetail} />
+      <Provider store={store}>
+        <BrowserRouter>
+          <div>
+            <Navbar />
+            <div className="container">
+              <Route
+                exact
+                path="/"
+                render={() => {
+                  return <Redirect to="rentals" />;
+                }}
+              />
+              <Route exact path="/rentals" component={RentalList} />
+              <Route exact path="/rentals/:id" component={RentalDetail} />
+            </div>
           </div>
-        </div>
-      </BrowserRouter>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
