ee08d6d1ec2f4512913ad20907cf13b57e6d6340
rjmcbain <rmcbain01@gmail.com>
2019-03-14 16:16:11 -0600

    adding node

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/rentals", function(req, res) {
+  res.json({ success: true });
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, function() {
+  console.log(`Server is running on port ${PORT}`);
+});
